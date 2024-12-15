5127525ad482d4d5848e2218b24cfb5ae9d4879d
AzizSaydullayev <azizbeksaydullayv66@gmail.com>
2024-12-15 17:40:34 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+function btn(id) {
+    switch (id) {
+        case 1:
+            alert("btn1")
+            break;
+        case 2:
+            alert("btn2")
+            break;
+        case 3:
+            alert("btn3")
+            break; 
+            case 4:
+            alert("btn4")
+            break; 
+            case 5:
+            alert("btn5")
+            break; 
+
+        default:
+            alert("boshka btn tanlang")
+            break;
+    }
+}
